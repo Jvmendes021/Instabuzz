1fb243af54a9deb6bf52ab88249bdc7cec3c7a6b
Jvmendes021 <joao.vitor13mendes@gmail.com>
2025-01-22 22:31:55 -0300

    Criando tela de login

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -1,42 +1,41 @@
-import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-
-import {Form,FormControl,FormDescription,FormField,FormItem,FormLabel,FormMessage,
-} from "@/components/ui/form"
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
-
-
 import { Button } from "@/components/ui/button"
-import { useForm } from "react-hook-form"
-
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+import { useForm, ControllerRenderProps } from "react-hook-form"
+import { SignupValidation } from "../../lib/validation"
+import { z } from "zod"
 
 const SignupForm = () => {
-  
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
-      username: "",
+      name: '',
+      username: '',
+      email: '',
+      password: '',
     },
   })
- 
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
+
   return (
-    <div>
-      <Form {...form}>
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col">
+        <img src="/assets/images/logo.svg" alt="logo" />
+      </div>
+
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
         <FormField
           control={form.control}
           name="username"
-          render={({ field }) => (
+          render={({ field }: { field: ControllerRenderProps<z.infer<typeof SignupValidation>, "username"> }) => (
             <FormItem>
               <FormLabel>Username</FormLabel>
               <FormControl>
@@ -52,7 +51,6 @@ const SignupForm = () => {
         <Button type="submit">Submit</Button>
       </form>
     </Form>
-    </div>
   )
 }
 
